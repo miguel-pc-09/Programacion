369f16d784b96d5a5757c794c0e02037717775fa
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-03-18 15:37:37 +0100

    update

---
+++ TEMA 2/SolucionEjerciciosT2/src/Ejercicio1.java
@@ -0,0 +1,15 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        final String NOMBRE = "Miguel Angel Perucha";
+        final String Direccion = "C. Budapest, s/n";
+        final int cp = 28890;
+        final String PROVINCIA = "Torrelodones";
+        final String PAIS = "España";
+
+        System.out.println(NOMBRE);
+        System.out.println(Direccion);
+        System.out.println(cp);
+        System.out.println(PROVINCIA);
+        System.out.println(PAIS);
+    }
+}
