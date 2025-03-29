3a7312e84034aef43f6fc50dbc53259835e29221
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-03-29 14:03:33 +0100

    update

--- TEMA 3/Estructuras/src/EntradaRepeticion.java
+++ TEMA 3/Estructuras/src/EntradaRepeticion.java
@@ -6,11 +6,11 @@ public class EntradaRepeticion {
     public static void main(String[] args) {
         // estructuraFor();
 
-        //
+        // estructuraWHILE();
 
-        //
+        // estructuraDOWHILE();
 
-        //
+        // ejecucionMENU();
     }
 
     public static void estructuraFor(){
@@ -24,6 +24,7 @@ public class EntradaRepeticion {
             System.out.println("Introduce numero");
             int numero = scanner.nextInt();
             sumatorio += numero;
+            // Cuantos de los numeros introducidos son pares
             if (numero % 2 == 0){
                 numeroPares++;
               }
@@ -35,6 +36,14 @@ public class EntradaRepeticion {
          */
 
         // tabla de multiplicar del 8
+        /*
+        for (int i = 0; i < 11; i++){
+            int resultado = i*8;
+            System.out.printf("%d * %d = %d\n", 8,i,8*i);
+        }*/
+
+
+        // Todas las tablas de multiplicar
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 11; j++) {
                 System.out.printf("%d * %d = %d\n",j,i,j*i);
@@ -44,8 +53,83 @@ public class EntradaRepeticion {
         System.out.println("Introduce el numero con el que juegas");
         int numero = scanner.nextInt();
         for (int i = 0; i < 5; i++) {
+              int aleatorio = (int)(Math.random()*30);
+            System.out.println(aleatorio); // Saca por consola los aleatorios que genera
+              if(numero == aleatorio){
+                  System.out.println("Numero adivinado");
+              }
 
         }
 
+    }
+
+    private static void estructuraWHILE(){
+        // repite mientras se cumpla la condiciones
+        int nota = scanner.nextInt();
+
+        while (nota<0 || nota>10){
+        System.out.println("Dime una nota ");
+            nota = scanner.nextInt();
+        }
+        System.out.println(nota);
+
+      /*  // while -> mientras.....repite
+        int nota = 4;
+        while(nota>0){
+            System.out.println("Ejecutando");
+            System.out.println("La nota es "+nota);
+            // para que no sea bucle infinito: que la condicion se modifique
+            // para que la nota llegue a cero y deje el bucle nota--; (Decimos que cada vez que ejecute reste 1)
+            nota--;
+        }*/
+    }
+
+    private static void estructuraDOWHILE(){
+        // primero ejecuta algo
+        // pregunta si la condicion de repeticion se cumple
+        int nota;
+        do{
+            System.out.println("Introduce una nota");
+            nota= scanner.nextInt();
+        }while(nota>0 || nota>10);
+    }
+
+    private static void ejecucionMENU(){
+        // Ejecucion cajero automatico
+        /*
+        * 1- Ingresar
+        * 2- sacar
+        * 3- consultar
+        * 4- salir
+        * que quieres hacer
+        * */
+        int opcion =0;
+        do{
+            System.out.println("1. Ingresar.");
+            System.out.println("2. Sacar.");
+            System.out.println("3. Consultar.");
+            System.out.println("4. Salir");
+            System.out.println("¿Que quieres hacer?");
+            // Recogemos la opcion con un scanner
+            opcion = scanner.nextInt();
+            // Ahora el switch
+            switch (opcion){
+                case 1:
+                    System.out.println("Te dispones a ingresar dinero");
+                    break;
+                case 2:
+                    System.out.println("Te dispones a sacar dineron.");
+                    break;
+                case 3:
+                    System.out.println("Consultando saldo");
+                    break;
+                case 4:
+                    System.out.println("Saliendo");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta, intentalo de nuevo");
+            }
+
+        }while(opcion !=4); // Si marca la opcion de salida
     }
 }
