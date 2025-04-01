c7c8efa1c9c0f5feb5131db92092c7a6e6557521
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-04-01 13:56:45 +0200

    update

---
+++ TEMA 4/ColeccionesEstaticas/src/Entrada.java
@@ -0,0 +1,14 @@
+public class Entrada {
+
+    public static void main(String[] args) {
+        // queriamos tener una variable antes
+        int numero = 5;
+        String palabra ="asdj";
+        // Pero si queremos que tenga mas datos creamos una array y tenemos 2 opciones
+        // 1º opcion es crear conjunto de posiciones
+        int [] numeros = new int [10 ];// el 10 reserva las posiciones
+        // 0 0 0 0 0 0 0 0 0 0 -> y son ceros porque son int
+        // si fuera String null null null null null null null null null null
+        // si fuera boolean Fallse False False False False False False False False False
+    }
+}
