fd5540f35f4821db7e55d8f882ec7f826e8f1730
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-04-25 19:43:39 +0200

    update

---
+++ TEMA 4/EjerciciosBaseT4/src/Entrada.java
@@ -0,0 +1,114 @@
+public class Entrada {
+    public static void main(String[] args) {
+        //ejercicioUno();
+        //ejercicioDos();
+        //ejercicioTres();
+        //ejercicioCuatro();
+        //ejercicioCinco();
+        //ejercicioSeis();
+        //ejercicioSiete();
+        //ArrayListUno();
+        //ArrayListDos();
+        //ArrayListTres();
+        //ArrayListCuatro();
+        //ArrayListCinco();
+        //ArrayListSeis();
+        //ArrayListSiete();
+        //ArrayListOcho();
+        //ArrayListNueve();
+        //ArrayListDiez();
+        
+    }
+    /*
+    *   ARRAYS
+    * */
+    /* Ejercicio1: Realiza un programa que pida 10 números por teclado y que los almacene en un array. A continuación
+     se mostrará el contenido de ese array junto al índice (0 – 9
+    */
+    public static void ejercicioUno(){}
+
+    /* Ejercicio 2: 2. Crear un programa que genere 30 números aleatorios entre 0 - 10 y los guarde en un array.
+     Una vez metidos, se deberá mostrar:
+    - Numero de puntos obtenidos
+    - Media de puntos obtenidos
+    */
+    public static void ejercicioDos(){}
+
+    /*Ejercicio 3: Escribe un programa que lea 10 números por teclado, los guarde en un array y que luego los muestre:
+    - en orden inverso, es decir, el primero que se introduce es el último en mostrarse
+    - en orden normal, es decir, el primero que se muestra es el primero que se ha introducido */
+    public static void ejercicioTres(){}
+
+    /*Ejercicio 4: 4. Define tres arrays de 20 números enteros cada una, con nombres numero, cuadrado y cubo.
+    Carga el array numero con valores aleatorios entre 0 y 100.
+    - En el array cuadrado se deben almacenar los cuadrados de los valores que hay en el array numero.
+    - En el array cubo se deben almacenar los cubos de los valores que hay en numero.
+    Una ver realizado lo anterior muestra el contenido de los tres arrays dispuesto en tres columnas.*/
+    public static void ejercicioCuatro(){}
+
+    /* Ejercicio 5:Realiza un programa que pida 8 números enteros, los guarde en un array y que luego muestre esos
+     números junto con la palabra “par” o “impar” según proceda. */
+    public static void ejercicioCinco(){}
+
+    /*Ejercicio 6:2. Escribe un programa que rellene un array de 20 números aleatorios entre el 0 y el 30.
+    Una vez hecho esto realizará las siguientes operaciones:
+    - modificar todos los 6 por 8
+    - modificar todos los 7 por 15
+    - modificar todos los 20 por 10
+Al finalizar el programa deberá indicar cuántas modificaciones se han realizado. */
+    public static void ejercicioSeis(){}
+
+    /*Ejercicio 7:Crea un programa que pida 10 palabras por consola y las guarde en un array de String. Una vez
+    hecho esto saldrá un menú que me dará las siguientes opciones:
+    - Ver todas las palabras: mostrarla todas las palabras del array
+    - Obtener una palabra al azar: mostrará una palabra de las que existen en el array
+    - Ver número de letras: mostrará el número de letras que hay en total entre todas las palabras
+    - Ver media de letras: mostrará el número medio de letras entre todas las palabas
+    - Ver palabra con más letras
+    - Ver palabra con menos letras */
+    public static void ejercicioSiete(){}
+
+    /*
+    * ARRAYLIST
+    * */
+    /*ArrayList1:1. Crea un ArrayList de String y realiza las siguientes acciones
+- Guarda manualmente 10 palabras
+- Imprime por pantalla todas las palabras del arraylist */
+    public static void ArrayListUno(){}
+
+    /*ArrayList2 : Crea un ArrayList de numeros enteros que guarde valores aleatorios (entre 0 y 100).
+    Una vez introducidos todos calcula la suma, la media, el máximo y el mínimo de esos números.*/
+    public static void ArrayListDos(){}
+
+    /*ArrayList3:Crea un programa que cree un ArrayList con los nombres de todos los compañeros de clase.
+    Una vez creado, sacará el nombre de uno de los compañeros (aleatorio) */
+    public static void ArrayListTres(){}
+
+    /*ArrayList4:Crea dos ArrayList de números y guarda de forma aleatorio 20 números sin decimales en cada uno de
+     ellos. Una vez hecho esto comprueba cuantos números de los dos arraylist son iguales (mismo número en la misma
+     posición). Tras hacer la comprobación indica cuantos números han sido iguales */
+    public static void ArrayListCuatro(){}
+
+    /*ArrayList5: Crea un programa que cree un ArrayList de palabras, introduce en él 10 palabras cualquiera.
+     Muestra el número de letras de todas las palabras, la palabra con más letras y la palabra con menos letra*/
+    public static void ArrayListCinco(){}
+
+    /*ArrayList6: Crea un programa que permita meter una lista de coches, donde cada coche consta de marca (String),
+    modelo (String) y caballos (int). Mete tres coches manualmente*/
+    public static void ArrayListSeis(){}
+
+    /*ArrayList7: Modifica el ejercicio anterior para que una vez metodos los coches se listen todos con el siguiente
+    formato: "Modelo: Ford	Marca: Focus	CV: 100"*/
+    public static void ArrayListSiete(){}
+
+    /*ArrayList8: Modifica el ejercicio anterior para que pida por teclado una marca y muestre por consola todos
+    los coches que sean de la marca introducida */
+    public static void ArrayListOcho(){}
+
+    /*ArrayList9:Modifica el ejercicio anterior para que pida por teclado una matricula y muestre el coche con
+    todos los datos. */
+    public static void ArrayListNueve(){}
+
+    /*ArrayList10:Crea un programa que permita realizar las funcionalidades de los ejercicions 5, 6 y 7 mediante un menú */
+    public static void ArrayListDiez(){}
+}
